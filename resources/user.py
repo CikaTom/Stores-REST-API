652f32aec06565ebca230a282b336f692860c362
Tomas Cachojanas <cikatom@gmail.com>
2020-07-27 13:05:54 +0200

    Added User resource

--- resources/user.py
+++ resources/user.py
@@ -26,3 +26,19 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created sucessfully"}, 201 #Created
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"Message": 'User not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"Message": 'User not found'}, 404
+        user.delete_from_db()
+        return {"Message": 'User deleted.'}, 200
